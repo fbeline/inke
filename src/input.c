ed6fd4f76139191b34138615bb5985cb70c00e4d
Felipe Beline Baravieira <felipe@fbeline.com>
2024-08-25 21:01:09 -0300

    refactor input file
    use "helper" functions to check if key is pressed / down / repeat

--- src/input.c
+++ src/input.c
@@ -8,29 +8,45 @@
 #include "fs.h"
 #include "utils.h"
 
+static bool kpr(int k) {
+  return IsKeyPressed(k) || IsKeyPressedRepeat(k);
+}
+
+static bool kctrl(void) {
+  return IsKeyDown(KEY_LEFT_CONTROL);
+}
+
+static bool kalt(void) {
+  return IsKeyDown(KEY_LEFT_ALT);
+}
+
+static bool kshift(void) {
+  return IsKeyDown(KEY_LEFT_SHIFT);
+}
+
 static void input_insert_handler(editor_t* E) { 
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_X)) {
+  if (kctrl() && IsKeyPressed(KEY_X)) {
     return mode_set_ctrl_x();
   }
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_A)) {
+  if (kctrl() && kpr(KEY_A)) {
     return cursor_bol();
   }
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_E)) {
+  if (kctrl() && kpr(KEY_E)) {
     return cursor_eol(E);
   }
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_K)) {
+  if (kctrl() && kpr(KEY_K)) {
     return cursor_delete_forward(E);
   }
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_Y)) {
+  if (kctrl() && kpr(KEY_Y)) {
     return cursor_insert_text(E, GetClipboardText());
   }
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_SPACE)) {
+  if (kctrl() && IsKeyPressed(KEY_SPACE)) {
     return cursor_region_start();
   }
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyDown(KEY_LEFT_SHIFT) && IsKeyPressed(KEY_BACKSPACE)) {
+  if (kctrl() && kshift() && IsKeyPressed(KEY_BACKSPACE)) {
     return cursor_delete_row(E);
   }
-  if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_W)) {
+  if (kalt() && IsKeyPressed(KEY_W)) {
     char* txt = cursor_region_text(E);
     if (txt != NULL) {
       SetClipboardText(txt);
@@ -39,31 +55,28 @@ static void input_insert_handler(editor_t* E) {
 
     cursor_clear_region();
   }
-  if (IsKeyDown(KEY_LEFT_ALT) && IsKeyDown(KEY_LEFT_SHIFT) && IsKeyPressed(KEY_COMMA)) {
+  if (kalt() && kshift() && kpr(KEY_COMMA)) {
     return cursor_bof();
   }
-  if (IsKeyDown(KEY_LEFT_ALT) && IsKeyDown(KEY_LEFT_SHIFT) && IsKeyPressed(KEY_PERIOD)) {
+  if (kalt() && kshift() && kpr(KEY_PERIOD)) {
     return cursor_eof(E);
   }
-  if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_F)) {
+  if (kalt() && kpr(KEY_F)) {
     return cursor_move_word_forward(E);
   }
-  if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_F)) {
-    return cursor_move_word_forward(E);
-  }
-  if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_B)) {
+  if (kalt() && kpr(KEY_B)) {
     return cursor_move_word_backward(E);
   }
-  if (IsKeyPressed(KEY_ENTER)) {
+  if (kpr(KEY_ENTER)) {
     return cursor_break_line(E);
   }
-  if (IsKeyPressed(KEY_PAGE_DOWN)) {
+  if (kpr(KEY_PAGE_DOWN)) {
     return cursor_page_down(E);
   }
-  if (IsKeyPressed(KEY_PAGE_UP)) {
+  if (kpr(KEY_PAGE_UP)) {
     return cursor_page_up(E);
   }
-  if (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_W)) {
+  if (kctrl() && IsKeyPressed(KEY_W)) {
     char* txt = cursor_region_kill(E);
     if (txt != NULL) {
       SetClipboardText(txt);
@@ -71,39 +84,34 @@ static void input_insert_handler(editor_t* E) {
     }
     cursor_clear_region();
   }
-  if (IsKeyPressed(KEY_RIGHT) || 
-    IsKeyPressedRepeat(KEY_RIGHT) ||
-    (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_F))) {
+  if (kpr(KEY_RIGHT) ||
+    (kctrl() && kpr(KEY_F))) {
     cursor_right(E);
   }
-  if (IsKeyPressed(KEY_DOWN) ||
-    IsKeyPressedRepeat(KEY_DOWN) ||
-    (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_N))) {
+  if (kpr(KEY_DOWN) ||
+    (kctrl() && kpr(KEY_N))) {
     cursor_down(E); 
   }
-  if (IsKeyPressed(KEY_LEFT) ||
-    IsKeyPressedRepeat(KEY_LEFT) ||
-    (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_B))) {
+  if (kpr(KEY_LEFT) ||
+    (kctrl() && kpr(KEY_B))) {
     cursor_left(E);
   }
-  if (IsKeyPressed(KEY_UP) ||
-    IsKeyPressedRepeat(KEY_UP) ||
-    (IsKeyDown(KEY_LEFT_CONTROL) && IsKeyPressed(KEY_P))) {
+  if (kpr(KEY_UP) ||
+    (kctrl() && kpr(KEY_P))) {
     cursor_up(E);
   }
-  if (IsKeyPressed(KEY_BACKSPACE)) {
+  if (kpr(KEY_BACKSPACE)) {
     cursor_remove_char(E);
   }
 
   i32 ch = GetCharPressed();
-  if (IN_RANGE(ch, 32, 95)) {
+  if (ch >= 32 && ch <= 126)
       cursor_insert_char(E, ch);
-  }
 }
 
 static void input_command_chain_handler(editor_t* E) {
-  if (IsKeyDown(KEY_LEFT_CONTROL)) {
-    if (IsKeyPressed(KEY_C)) {
+  if (kctrl()) {
+    if (kpr(KEY_C)) {
       if (E->dirty) {
         g_mode = COMMAND_CHAIN;
         mode_set_exit_save(E);
@@ -112,14 +120,14 @@ static void input_command_chain_handler(editor_t* E) {
       }
       return;
     }
-    if (IsKeyPressed(KEY_S)) {
+    if (kpr(KEY_S)) {
       char* buf = editor_rows_to_string(E->rows, E->row_size);
       FileWrite(E->filename, buf);
       free(buf);
       E->dirty = false;
       mode_cmd_clean();
     }
-    if (IsKeyPressed(KEY_G)) {
+    if (kpr(KEY_G)) {
       return mode_cmd_clean();
     }
   }
@@ -127,9 +135,8 @@ static void input_command_chain_handler(editor_t* E) {
 
 static void input_command_char_handler(editor_t* E) {
   i32 ch = GetCharPressed();
-  if (IN_RANGE(ch, 32, 95)) {
+  if (ch >= 32 && ch <= 126)
     g_active_command.handler(E, ch);
-  }
 }
 
 void input_keyboard_handler(editor_t* E) {
