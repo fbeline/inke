eeaf730ce2cf445089a3071c1296e0b9743972b4
Felipe Beline Baravieira <felipe@fbeline.com>
2024-09-03 11:49:30 -0300

    test: cursor position

--- tests/ctest.h
+++ tests/ctest.h
@@ -9,9 +9,19 @@
         return 1; \
     }
 
+
+#define ASSERT_VEC2_EQUAL(expected_x, expected_y, actual) \
+    if ((expected_x) != (actual.x) || (expected_y) != (actual.y)) { \
+        printf("Test failed: (%s, %s) == %s, expected (%d, %d) but got (%d, %d)\n", \
+               #expected_x, #expected_y, #actual, expected_x, expected_y, actual.x, actual.y); \
+        return 1; \
+    }
+
 #define ASSERT_STRING_EQUAL(expected, actual) \
     if (strcmp((expected), (actual)) != 0) { \
         printf("Test failed: %s == %s\nexpected=\"%s\"\nresult=\"%s\"\n", #expected, #actual, (expected), (actual)); \
         return 1; \
     }
+
+
 
--- tests/cursor_test.c
+++ tests/cursor_test.c
@@ -1,10 +1,57 @@
+#include <stdlib.h>
+#include <string.h>
 #include "../src/cursor.h"
 #include "ctest.h"
 
+editor_t editor_factory() {
+   editor_t e = {
+    .mode = 0,
+    .filename = "foo.txt",
+    .row_size = 2,
+    .row_capacity = 2,
+    .dirty = false,
+    .new_file = false,
+    .running = true,
+  };
+
+  e.rows = malloc(sizeof(row_t) * 2);
+
+  char* t1 = "foo bar baz";
+  e.rows[0].size = strlen(t1);
+  e.rows[0].chars = strdup(t1);
+
+  char* t2 = "qux quux corge";
+  e.rows[1].size = strlen(t2);
+  e.rows[1].chars = strdup(t2);
+
+  return e;
+}
+
 cursor_t factory() {
-  return (cursor_t){0};
+  editor_t E = editor_factory();
+  cursor_t C = {0};
+  C.editor = malloc(sizeof(editor_t));
+  memcpy(C.editor, &E, sizeof(editor_t));
+
+  return C;
+}
+
+static int test_cursor_position(void) {
+  cursor_t C = factory();
+
+  ASSERT_VEC2_EQUAL(0, 0, cursor_position(&C));
+
+  C.x = 10; C.coloff = 5;
+  C.y = 3; C.rowoff = 20;
+  ASSERT_VEC2_EQUAL(16, 23, cursor_position(&C));
+
+  return 0;
 }
 
 int main() {
-  return 0;
+  int result = 0;
+
+  result += test_cursor_position();
+
+  return result;
 }
