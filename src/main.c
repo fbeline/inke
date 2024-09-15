f770ec0445453752cbc1d3c7b7a2d7a9170fbec4
Felipe Beline Baravieira <felipe@fbeline.com>
2024-09-15 11:51:09 -0300

    basic code to run editor in terminal raw mode

--- src/main.c
+++ src/main.c
@@ -1,16 +1,42 @@
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include "editor.h"
 #include "cursor.h"
+#include "utils.h"
 
 static editor_t E = {0};
 static cursor_t C = {0};
+struct termios orig_termios;
 
 void Init(const char* filename) {
   E = editor_init(filename);
   C = cursor_init(&E);
+}
+
+static void disable_raw_mode(void) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+static void enable_raw_mode(void) {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  atexit(disable_raw_mode);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    die("tcsetattr");
 }
 
 int main(int argc, char **argv) {
@@ -20,6 +46,18 @@ int main(int argc, char **argv) {
   }
 
   Init(argv[1]);
+  enable_raw_mode();
+
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
 
   return 0;
 }
