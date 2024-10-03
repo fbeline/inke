156cffd66555d1c19168096917224efa38dddb76
Felipe Beline Baravieira <felipe@fbeline.com>
2024-10-03 20:58:38 -0300

    refactor isearch forward

--- src/isearch.c
+++ src/isearch.c
@@ -6,36 +6,38 @@
 #include "cursor.h"
 #include "globals.h"
 
-static cursor_t ocursor = {0};
-
 static void isearch_forward(cursor_t *C, const char *query) {
   line_t *l = C->clp;
   u32 ocy = C->y + C->rowoff;
   u32 offset = C->x + C->coloff;
-
-  g_isearch = (isearch_t){.x = 0, .y = ocy, .qlen = 0};
+  u32 y = 0;
 
   while (l != NULL) {
     char *match = strstr(l->ds->buf + offset, query);
-    if (match) {
+    if (match == l->ds->buf + offset) {
+      offset += strlen(query);
+      continue;
+    } else if (match) {
       g_isearch.x = match - (l->ds->buf + offset);
       g_isearch.qlen = strlen(query);
       break;
     }
-    l = l->next;
-    g_isearch.y++;
+
     offset = 0;
+    y++;
+    l = l->next;
   }
 
   if (l == NULL) return;
 
-  for (u32 i = 0, max = g_isearch.y - ocy; i < max; i++) {
+  cursor_bol(C);
+  for (u32 i = 0; i < y; i++)
     cursor_down(C);
-    cursor_bol(C);
-  }
 
-  for (u32 i = 0, max = g_isearch.x - offset; i < max; i++)
+  for (u32 i = 0; i < g_isearch.x; i++)
     cursor_right(C);
+
+  g_isearch.y = ocy + y;
 }
 
 static void isearch_reverse(cursor_t *C, const char *query) {
@@ -43,7 +45,8 @@ static void isearch_reverse(cursor_t *C, const char *query) {
 }
 
 static void isearch_return(cursor_t *C) {
-  // TODO
+  g_mode = MODE_INSERT;
+  clear_status_message();
 }
 
 void isearch(cursor_t *C, int opt) {
@@ -62,8 +65,6 @@ void isearch(cursor_t *C, int opt) {
 }
 
 void isearch_start(cursor_t *C) {
-  ocursor = *C;
-
   cmdline_init("I-Search: ");
   g_mode = MODE_SEARCH;
   g_cmd_func = isearch;
